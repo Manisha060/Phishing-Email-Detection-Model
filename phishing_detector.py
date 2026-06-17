ae126afde15c5dc08ea80897e002f7644de6c8d2
Manisha060 <manishay8778@gmail.com>
2026-06-17 11:13:06 +0530

    phishing Email Detection Model

---
+++ phishing_detector.py
@@ -0,0 +1,33 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import accuracy_score, confusion_matrix
+
+# Load dataset
+data = pd.read_csv("phishing_email.csv")
+
+# Features and Labels
+X = data["text_combined"]
+y = data["label"]
+
+# Text to numerical features
+vectorizer = TfidfVectorizer(stop_words='english')
+X = vectorizer.fit_transform(X)
+
+# Split dataset
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+# Train model
+model = MultinomialNB()
+model.fit(X_train, y_train)
+
+# Predict
+y_pred = model.predict(X_test)
+
+# Results
+print("Accuracy:", accuracy_score(y_test, y_pred))
+print("\nConfusion Matrix:")
+print(confusion_matrix(y_test, y_pred))
